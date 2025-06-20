76d8357d26ea093d952c34c7c22532f965ac3334
Lizwi <Lizwi>
2025-06-20 14:13:29 +0200

    cast create_at date on client model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Client extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $appends = ['created_at_formatted'];
 
     protected $fillable = [
         'first_name',
@@ -21,5 +24,13 @@ class Client extends Model
     public function creator()
     {
         return $this->belongsTo(User::class, 'created_by');
+    }
+
+
+    public function createdAtFormatted(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->created_at?->format('Y-m-d'),
+        );
     }
 }
